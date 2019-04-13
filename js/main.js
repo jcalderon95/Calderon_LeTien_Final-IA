080dc115cf47c45e850f35b88efe3ac5caf5c01b
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2019-04-13 05:22:00 -0400

    finished scrolling function

--- js/main.js
+++ js/main.js
@@ -5,14 +5,18 @@
 	
 	console.log("fired!!!");
 
-	var button = document.querySelector("#button");
-	var burgerCon = document.querySelector("#burgerCon");
-	var accordions = document.querySelectorAll(".accordion");
+	var button      =  document.querySelector("#button");
+	var burgerCon   =  document.querySelector("#burgerCon");
+	var accordions  =  document.querySelectorAll(".accordion");
+	var welcomeSection = document.querySelector(".welcomeCon");
+	var links   	=  document.querySelectorAll("#mainNav a");
+	var footerLinks =  document.querySelectorAll(".footerNav a");
 
 	function hamburgerMenu() {
 		burgerCon.classList.toggle("slideToggle");
 		button.classList.toggle("expanded");
 	}
+
 
 	function closeMenu() {
 
@@ -29,6 +33,15 @@
 		e.currentTarget.nextElementSibling.classList.toggle('isOpen');
 	}
 
+	function scrollLink(e){
+		// console.log("links fired");
+		e.preventDefault();
+		let targetArea = e.currentTarget.id.slice(0,-3);
+		TweenLite.to(window, 1, {scrollTo:{y:`#${targetArea}`, offsetY:50, autoKill:false}});
+	}
+
+	TweenLite.from(welcomeSection, 2, {y:40, opacity: 0, delay: 0.6});
+
 
 	button.addEventListener("click", hamburgerMenu, false);
 
@@ -36,5 +49,8 @@
 
 	accordions.forEach(accordion => accordion.addEventListener('click', openClose));
 
+	links.forEach(link => link.addEventListener('click', scrollLink));
+
+	footerLinks.forEach(link => link.addEventListener('click', scrollLink));
 		
 })();
